2866774fc5674aa7e7bd6841f929e56aaee71416
chungchung <hangjastar@gmail.com>
2021-02-09 21:27:47 +0900

    뒤끝 서버 세팅

---
+++ fps_game/Assets/backend/server.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BackEnd;
+
+public class server : MonoBehaviour
+{
+    void Start()
+    {
+        var bro = Backend.Initialize();
+
+        if (bro.IsSuccess())
+        {
+            Debug.Log("Success");
+        }
+        else
+        {
+            Debug.Log("Fail");
+        }
+    }
+}
